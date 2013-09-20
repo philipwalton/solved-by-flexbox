6bab4e8c1eee29c14bc6b857f397ab491d20d9ba
Philip Walton <philip@philipwalton.com>
2013-09-19 21:15:54 -0700

    Add an autoprefixer support placeholder.

--- config.rb
+++ config.rb
@@ -1,3 +1,13 @@
+# require 'autoprefixer-rails'
+# require 'csso'
+
+# on_stylesheet_saved do |file|
+#   css = File.read(file)
+#   File.open(file, 'w') do |io|
+#     io << Csso.optimize( AutoprefixerRails.compile(css) )
+#   end
+# end
+
 require 'sass-css-importer'
 add_import_path Sass::CssImporter::Importer.new("./bower_components/")
 
