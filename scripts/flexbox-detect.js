d06599bed6e3e86fcfc348542023501bcbfde9c4
Philip Walton <philip@philipwalton.com>
2014-05-07 18:50:37 -0700

    Use local body reference instead of document.body.

--- scripts/flexbox-detect.js
+++ scripts/flexbox-detect.js
@@ -1,9 +1,8 @@
-;(function() {
-  var div, body = document.body
-  if (body.style.flex == null && document.body.style.webkitFlex == null) {
+;(function(body, div) {
+  if (body.style.flex == null && body.style.webkitFlex == null) {
     div = document.createElement("div")
     div.className = "Error"
     div.innerHTML = "Your browser does not support Flexbox. Parts of this site may not appear as expected."
     body.insertBefore(div, body.firstChild)
   }
-}())
+}(document.body))
