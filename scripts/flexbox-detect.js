6db7197511b35bb5ddbc18875ac55c01db7d6546
Philip Walton <philip@philipwalton.com>
2014-11-20 20:49:09 -0800

    Update the flexbox support check to include IE10.

--- scripts/flexbox-detect.js
+++ scripts/flexbox-detect.js
@@ -1,5 +1,5 @@
 ;(function(body, div) {
-  if (body.style.flex == null && body.style.webkitFlex == null) {
+  if (!('flexBasis' in style || 'msFlexAlign' in style)) {
     div = document.createElement("div")
     div.className = "Error"
     div.innerHTML = "Your browser does not support Flexbox. Parts of this site may not appear as expected."
