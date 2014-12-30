a5bdd1a037d8dbbede4502c16e7e0166cd7af24a
Philip Walton <philip@philipwalton.com>
2014-12-30 15:10:44 -0800

    Site deployed at 2014-12-30 23:10:44 UTC

--- scripts/track-events.js
+++ scripts/track-events.js
@@ -1,44 +1,44 @@
 // track clicks on outbound hyperlinks
-;(function() {
+(function() {
 
-  var links = document.getElementsByTagName("a")
-    , link
-    , i = 0
-    , ga = window.ga
+  var links = document.getElementsByTagName('a');
+  var link;
+  var i = 0;
+  var ga = window.ga;
 
   function isLink(el) {
-    return el.nodeName.toLowerCase() == "a" && el.href
+    return el.nodeName.toLowerCase() == 'a' && el.href;
   }
 
   function isExternalLink(el) {
-    return el.href.indexOf(location.host) < 0
+    return el.href.indexOf(location.host) < 0;
   }
 
   function getLinkAncestor(el) {
-    if (isLink(el)) return el
+    if (isLink(el)) return el;
     while (el.parentNode && el.parentNode.nodeType == 1) {
-      if (isLink(el)) return el
-      el = el.parentNode
+      if (isLink(el)) return el;
+      el = el.parentNode;
     }
   }
 
   function logExternalClicks(event) {
-    var e = event || window.event
-      , target = e.target || e.srcElement
-      , link = getLinkAncestor(target)
+    var e = event || window.event;
+    var target = e.target || e.srcElement;
+    var link = getLinkAncestor(target);
     if (link && isExternalLink(link)) {
-      ga && ga("send", "event", "Outbound Link", link.href);
+      ga && ga('send', 'event', 'Outbound Link', link.href);
     }
   }
 
-  // add target="_blank" to external links
+  // add target='_blank' to external links
   while(link = links[i++]) {
-    if (link.target != "_blank" && isExternalLink(link)) {
-      link.target = "_blank"
+    if (link.target != '_blank' && isExternalLink(link)) {
+      link.target = '_blank';
     }
   }
 
   // register logging on click events
-  document.addEventListener("click", logExternalClicks)
+  document.addEventListener('click', logExternalClicks);
 
-}())
+}());
