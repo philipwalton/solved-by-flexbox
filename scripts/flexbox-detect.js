59fb7d0c455045f418beb35b0f243ba28f815273
Philip Walton <philip@philipwalton.com>
2014-12-29 22:20:46 -0600

    Add a .jshintrc file and standardize the JavaScript style.

--- scripts/flexbox-detect.js
+++ scripts/flexbox-detect.js
@@ -1,10 +1,13 @@
-;(function(body, div) {
+(function(body) {
   if (!('flexBasis' in body.style ||
         'webkitFlexBasis' in body.style ||
         'msFlexAlign' in body.style)) {
-    div = document.createElement("div")
-    div.className = "Error"
-    div.innerHTML = "Your browser does not support Flexbox. Parts of this site may not appear as expected."
-    body.insertBefore(div, body.firstChild)
+
+    var div = document.createElement('div');
+    div.className = 'Error';
+    div.innerHTML = 'Your browser does not support Flexbox. ' +
+                    'Parts of this site may not appear as expected.';
+
+    body.insertBefore(div, body.firstChild);
   }
-}(document.body))
+}(document.body));
