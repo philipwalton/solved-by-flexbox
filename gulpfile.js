1d4a6e66912c6b23ac2e3f15624833a0c5fd3dce
Philip Walton <philip@philipwalton.com>
2015-01-24 14:59:49 -0800

    Add gulpfile and package.json.

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,125 @@
 var assign = require('object-assign');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var connect = require('connect');
+var cssnext = require('gulp-cssnext');
+var del = require('del');
 var frontMatter = require('front-matter');
 var gulp = require('gulp');
-var cssnext = require('gulp-cssnext');
+var gulpIf = require('gulp-if');
 var gutil = require('gulp-util');
 var he = require('he');
 var hljs = require('highlight.js');
+var jshint = require('gulp-jshint');
 var nunjucks = require('nunjucks');
 var path = require('path');
+var plumber = require('gulp-plumber');
 var Remarkable = require('remarkable');
 var rename = require('gulp-rename');
-var through = require('through2');
-
-
-var browserify = require('browserify');
-var buffer = require('vinyl-buffer');
-var jshint = require('gulp-jshint');
+var serveStatic = require('serve-static');
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
+var through = require('through2');
 var uglify = require('gulp-uglify');
 
-var gulpIf = require('gulp-if');
+
+/**
+ * The output directory for all the built files.
+ */
+var buildDir = './build';
+
+
+nunjucks.configure('templates', { autoescape: false });
+
+
+function streamError(err) {
+  gutil.beep();
+  gutil.log(err instanceof gutil.PluginError ? err.toString() : err.stack);
+}
+
 
 function isProd(transform) {
   // Assume prod unless NODE_ENV starts with 'dev'.
   return gulpIf(!/^dev/.test(process.env.NODE_ENV), transform);
 }
 
-nunjucks.configure('templates', { autoescape: false });
-
-
-var markdown = new Remarkable({
-  html: true,
-  breaks: false,
-  typographer: true,
-  highlight: function (code, lang) {
-    return lang ? hljs.highlight(lang, code).value : he.escape(code);
-  }
-});
-
 
 function extractFrontMatter() {
   return through.obj(function (file, enc, cb) {
-    var data = frontMatter(file.contents.toString());
-    var content = data.body.trim();
-    file.data = data.attributes;
-    file.contents = new Buffer(content);
-    this.push(file);
+    try {
+      var data = frontMatter(file.contents.toString());
+      var content = data.body.trim();
+      file.data = data.attributes;
+      file.contents = new Buffer(content);
+      this.push(file);
+    }
+    catch (err) {
+      this.emit('error', new gutil.PluginError('pages', err, {
+        fileName: file.path
+      }));
+    }
     cb();
   });
 }
 
 
 function renderMarkdown() {
-  return through.obj(function (file, enc, cb) {
-    if (path.extname(file.path) == '.md') {
-      file.contents = new Buffer(markdown.render(file.contents.toString()));
+  var markdown = new Remarkable({
+    html: true,
+    typographer: true,
+    highlight: function (code, lang) {
+      // Unescape to avoid double escaping.
+      code = he.unescape(code);
+      return lang ? hljs.highlight(lang, code).value : he.escape(code);
     }
-    this.push(file);
+  });
+  return through.obj(function (file, enc, cb) {
+    try {
+      if (path.extname(file.path) == '.md') {
+        file.contents = new Buffer(markdown.render(file.contents.toString()));
+      }
+      this.push(file);
+    }
+    catch (err) {
+      this.emit('error', new gutil.PluginError('pages', err, {
+        fileName: file.path
+      }));
+    }
     cb();
   });
 }
 
 
 function renderTemplate() {
+  var globalData =  {
+    env: /^dev/.test(process.env.NODE_ENV) ? 'dev' : 'prod'
+  };
   return through.obj(function (file, enc, cb) {
-    var template = file.data.template;
-    var templateData = assign({env: 'dev'}, file.data);
-    var content = file.contents.toString();
+    try {
+      var template = file.data.template;
+      var templateData = assign(globalData, file.data);
+      var content = file.contents.toString();
 
-    // Render the content with the local data before rendering the template
-    // with the full site data.
-    templateData.content = nunjucks.renderString(content, templateData);
+      // Render the content with the local data before rendering the template
+      // with the full site data.
+      templateData.content = nunjucks.renderString(content, templateData);
 
-    file.contents = new Buffer(nunjucks.render(template, templateData));
-    this.push(file);
+      file.contents = new Buffer(nunjucks.render(template, templateData));
+      this.push(file);
+    }
+    catch (err) {
+      this.emit('error', new gutil.PluginError('pages', err, {
+        fileName: file.path
+      }));
+    }
     cb();
   });
 }
 
 
-
 gulp.task('pages', function() {
   gulp.src(['*.html', './demos/**/*'], {base: process.cwd()})
+      .pipe(plumber({errorHandler: streamError}))
       .pipe(extractFrontMatter())
       .pipe(renderMarkdown())
       .pipe(renderTemplate())
@@ -92,51 +130,57 @@ gulp.task('pages', function() {
           path.extname = '.html';
         }
       }))
-      .pipe(gulp.dest('./build/'));
+      .pipe(gulp.dest(buildDir));
 });
 
 
 gulp.task('images', function() {
   gulp.src('./assets/images/**/*')
-      .pipe(gulp.dest('./build/images/'));
+      .pipe(gulp.dest(path.join(buildDir, 'images')));
 });
 
 
 gulp.task('css', function() {
   gulp.src('./assets/css/main.css')
-      .pipe(cssnext(/*{compress: true}*/))
-      .pipe(gulp.dest('./build/'));
+      .pipe(plumber({errorHandler: streamError}))
+      .pipe(cssnext({compress: true}))
+      .pipe(gulp.dest(buildDir));
 });
 
 
 gulp.task('lint', function() {
   gulp.src('./assets/javascript/**/*.js')
+      .pipe(plumber({errorHandler: streamError}))
       .pipe(jshint())
       .pipe(jshint.reporter('default'))
-      .pipe(isProd(jshint.reporter('fail')));
+      .pipe(isProd(jshint.reporter('fail')))
 });
 
 
-gulp.task('javascript', function() {
+gulp.task('javascript', ['lint'], function() {
   browserify('./assets/javascript/main.js', {debug: true}).bundle()
+      .on('error', streamError)
       .pipe(source('main.js'))
       .pipe(buffer())
       .pipe(sourcemaps.init({loadMaps: true}))
       .pipe(isProd(uglify()))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./build/'));
+      .pipe(gulp.dest(buildDir));
 });
 
 
-var allTasks = ['css', 'images', 'javascript', 'pages', 'lint'];
+gulp.task('clean', function() {
+  del(buildDir);
+});
 
 
-gulp.task('watch', allTasks, function() {
+gulp.task('default', ['clean', 'css', 'images', 'javascript', 'pages']);
+
+
+gulp.task('serve', ['default'], function() {
+  connect().use(serveStatic(buildDir)).listen(3000);
   gulp.watch('./assets/css/**/*.css', ['css']);
   gulp.watch('./assets/images/*', ['images']);
   gulp.watch('./assets/javascript/*', ['javascript']);
   gulp.watch(['*.html', './demos/*', './templates/*'], ['pages']);
 });
-
-
-gulp.task('default', allTasks);
