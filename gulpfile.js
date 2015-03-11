cbc3e73f76d9c11e24e2f7880c5d6223e148e95f
Philip Walton <philip@philipwalton.com>
2015-03-11 16:50:20 -0700

    Be more explicit about the autoprefixed browsers.

--- gulpfile.js
+++ gulpfile.js
@@ -181,7 +181,11 @@ gulp.task('images', function() {
 gulp.task('css', function() {
   return gulp.src('./assets/css/main.css')
       .pipe(plumber({errorHandler: streamError}))
-      .pipe(cssnext({compress: true}))
+      .pipe(cssnext({
+        browsers: '> 1%, last 2 versions, Safari > 5, ie > 9, Firefox ESR',
+        compress: true,
+        url: false
+      }))
       .pipe(gulp.dest(DEST));
 });
 
